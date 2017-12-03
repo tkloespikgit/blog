eb0819ca741513df1678ff23dc2dbdb39129a910
tkloespik <tkloespik@163.com>
2017-12-03 22:49:57 +0800

    modify html

--- resources/views/layouts/homeLayout.blade.php
+++ resources/views/layouts/homeLayout.blade.php
@@ -40,18 +40,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" style="height: 1px;">
             <ul class="nav navbar-nav">
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Jobs<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="location.html">Contract Jobs</a></li>
-                        <li><a href="location.html">Walkin Jobs</a></li>
-                        <li><a href="location.html">Jobs by Location</a></li>
-                        <li><a href="location.html">Jobs by Function</a></li>
-                        <li><a href="location.html">Jobs by Industry</a></li>
-                        <li><a href="location.html">Jobs by Company</a></li>
-                    </ul>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">主页</a>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">文章<b class="caret"></b></a>
                     <ul class="dropdown-menu multi-column columns-3">
                         <div class="row">
                             <div class="col-sm-4">
@@ -91,7 +83,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Recruiters<b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">问答<b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a name='login'>Recruiter Updates</a></li>
                         <li><a href="recruiters.html">Recruiters you are following</a></li>
@@ -99,24 +91,16 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                     </ul>
                 </li>
                 <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">More<b class="caret"></b></a>
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">邮件<b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><a href="jobs.html">Walk-ins</a></li>
                         <li><a href="jobs.html">Bpo Jobs</a></li>
                         <li><a href="jobs.html">Teaching Jobs</a></li>
                         <li><a href="jobs.html">Diploma Jobs</a></li>
-                        <li><a href="jobs.html">Tech Support</a></li>
-                        <li><a href="jobs.html">Finance Jobs</a></li>
-                        <li><a href="jobs.html">Part time Jobs</a></li>
-                        <li><a href="jobs.html">Health Care</a></li>
-                        <li><a href="jobs.html">Hospitality</a></li>
-                        <li><a href="jobs.html">Internships</a></li>
-                        <li><a href="jobs.html">Research Jobs</a></li>
-                        <li><a href="jobs.html">Defence Jobs</a></li>
                     </ul>
                 </li>
-                <li><a name='login'>Login</a></li>
-                <li><a href="resume.html">Upload Resume</a></li>
+                <li><a name='login'>登录</a></li>
+                <li><a href="resume.html">个人中心</a></li>
             </ul>
         </div>
         <div class="clearfix"></div>
@@ -127,13 +111,10 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <div class="container">
         <div id="search_wrapper1">
             <div id="search_form" class="clearfix">
-                <h1>Start your job search</h1>
+                <h1>搜索文章</h1>
                 <p>
-                    <input type="text" class="text" placeholder=" " value="Enter Keyword(s)" onfocus="this.value = '';"
-                           onblur="if (this.value == '') {this.value = 'Enter Keyword(s)';}">
-                    <input type="text" class="text" placeholder=" " value="Location" onfocus="this.value = '';"
-                           onblur="if (this.value == '') {this.value = 'Location';}">
-                    <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Find Jobs"></label>
+                    <input type="text" class="text" placeholder="请输入关键词 .." value="" name="keywords">
+                    <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Search"></label>
                 </p>
             </div>
         </div>
@@ -142,48 +123,14 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 @yield('content')
 <div class="footer">
     <div class="container">
-        <div class="col-md-3 grid_3">
-            <h4>Navigate</h4>
-            <ul class="f_list f_list1">
-                <li><a href="index.html">Home</a></li>
-                <li><a name='login'>Sign In</a></li>
-                <li><a name='login'>Join Now</a></li>
-                <li><a href="about.html">About</a></li>
-            </ul>
-            <ul class="f_list">
-                <li><a href="features.html">Features</a></li>
-                <li><a href="terms.html">Terms of use</a></li>
-                <li><a href="contact.html">Contact Us</a></li>
-                <li><a href="jobs.html">Post a Job</a></li>
-            </ul>
-            <div class="clearfix"></div>
-        </div>
-        <div class="col-md-3 grid_3">
-            <h4>Twitter Widget</h4>
-            <div class="footer-list">
-                <ul>
-                    <li><i class="fa fa-twitter tw1"> </i>
-                        <p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-                    <li><i class="fa fa-twitter tw1"> </i>
-                        <p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-                    <li><i class="fa fa-twitter tw1"> </i>
-                        <p><span class="yellow"><a href="#">consectetuer</a></span> adipiscing elit web design</p></li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-md-3 grid_3">
-            <h4>Seeking</h4>
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
-                in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-        </div>
-        <div class="col-md-3 grid_3">
-            <h4>Sign up with your account or email</h4>
-            {{--<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.</p>--}}
+        <div class="col-md-4 grid_3">
+            <h4>输入您的邮箱或帐号登录！</h4>
             <form>
                 <a href="#login"></a>
-                <input type="text" class="form-control" placeholder="Enter your email or account">
+                <input type="text" class="form-control"  placeholder="Enter your email or account">
                 <input type="text" class="form-control" placeholder="Enter your password">
-                <button type="button" class="btn red">Sign Up</button>
+                <button type="button" class="btn red"> 立 即 登 录 </button>
+                <a href="register.html" class="btn red"> 前 往 注 册 </a>
             </form>
         </div>
         <div class="clearfix"></div>
@@ -191,7 +138,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 </div>
 <div class="footer_bottom">
     <div class="container">
-        <div class="col-sm-2">
+        {{--<div class="col-sm-2">
             <ul class="f_list2">
                 <li><a href="jobs.html">Russia Jobs</a></li>
                 <li><a href="jobs.html">Australia Jobs</a></li>
@@ -222,9 +169,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                 consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
                 dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numqua"</p>
         </div>
-        <div class="clearfix"></div>
+        <div class="clearfix"></div>--}}
         <div class="copy">
-            <p>Tkloespik &copy; 2017-12.blog<a href="http://www.cssmoban.com/" target="_blank" title="fuldamall">富达商城</a></p>
+            <p>Tkloespik &copy; 2017-12.blog<a href="https://www.fuldamall.com/" target="_blank" title="fuldamall">-后台链接</a></p>
         </div>
     </div>
 </div>
